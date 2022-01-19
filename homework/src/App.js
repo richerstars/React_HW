b4a14d065f67993743d189cb3d0922d622f7166c
richerstars <81115334+richerstars@users.noreply.github.com>
2022-01-19 11:59:39 +0200

    add todo fetch

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 import Form from "./components/Form/Form";
 import List from "./components/List/List";
 import './App.css';
+import Loader from "./components/Loader/Loader";
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {todos: [{value: 'test', id: 1, isChecked: false}]};
+        this.state = {todos: [{value: 'test', id: 10000, isChecked: false}]};
+    }
+
+    componentDidMount() {
+        this.getDateFromApi();
     }
 
     addListItem = (values) => {
@@ -18,7 +24,21 @@ class App extends Component {
     }
 
     changeChecked = (id) => {
-        this.setState({todos: this.state.todos.map(el => el.id === id ? {...el, isChecked: !el.isChecked} : el)});
+        this.setState({
+            todos: this.state.todos.map(el => el.id === id ? {
+                ...el,
+                isChecked: !el.isChecked
+            } : el)
+        });
+    }
+
+    getDateFromApi = async () => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
+            console.log(response.data);
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     render() {
--- homework/src/components/List/List.jsx
+++ homework/src/components/List/List.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ListItem from "./ListItem/ListItem";
+import './List.css'
+
 
 const List = ({todos, changeTodo, deleteTodo}) => (
         <div className="todos">
