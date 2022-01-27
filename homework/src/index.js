13212768e742abb489d974b372c691e9a2c8c1f2
Andrew_Laptop <superguper2012@gmail.com>
2022-01-27 13:33:50 +0200

    some class adds

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AudioPlayer from '../src/components/AudioPlayer'
+import AudioPlayer from '../src/components/AudioPlayer';
+import AudioPlayerClass from '../src/components/AudioPlayerClass';
 import tracks from "./tracks";
 
 ReactDOM.render(
-    <AudioPlayer tracks={tracks}/>,
+    // <AudioPlayer tracks={tracks}/>,
+    <AudioPlayerClass tracks={tracks}/>,
   document.getElementById('root')
 );
