97eab4a81c3cb8f5ab37ee03986c88a8929aba4b
Andrew_Laptop <superguper2012@gmail.com>
2022-02-07 14:14:49 +0200

    useReducer

--- homework/src/context/UserContext.jsx
+++ homework/src/context/UserContext.jsx
@@ -1,16 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 
+const initialState = {user: 'Guest'};
 const UserContext = React.createContext();
+const SET_USERNAME = 'SET_USERNAME';
+
+const reducer =(state, {payload,type}) =>{
+    switch (type){
+        case SET_USERNAME:
+            return {user: payload || 'Guest'};
+        default:
+            return state;
+    }
+}
 
 const UserContextProvider = ({children}) => {
-    const [user, setUser] = useState("Guest");
-    const setUsername = (name)=> {
-        setUser(name || "Guest")
-    }
-    const contextValue = {user,setUsername }
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const setUsername = (name)=> dispatch({type: SET_USERNAME, payload:name })
+    const {user} = state;
+    const contextValue = { user,setUsername }
     return (
         <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
     );
+
 };
 
 export {UserContextProvider, UserContext};
